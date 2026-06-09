4afcb48008acf4a3e87abf08a70a7a916fa72556
qkrrms0-blip <qkrrms0@gmail.com>
2026-06-09 15:34:37 +0900

    sw 핸들러 삭제

--- public/sw.js
+++ public/sw.js
@@ -29,10 +29,3 @@ self.addEventListener("notificationclick", (event) => {
     })
   );
 });
-
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-  event.respondWith(
-    fetch(event.request).catch(() => new Response("", { status: 503 }))
-  );
-});
